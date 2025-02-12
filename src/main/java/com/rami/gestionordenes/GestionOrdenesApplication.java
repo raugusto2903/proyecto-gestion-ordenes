7103f677cad86123d05999997d24a6c489309a30
raugusto2903 <ramirol@titamedia.com>
2025-02-12 17:02:08 -0500

    Moviendo gestion-ordenes a la raíz del repositorio

---
+++ src/main/java/com/rami/gestionordenes/GestionOrdenesApplication.java
@@ -0,0 +1,13 @@
+package com.rami.gestionordenes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionOrdenesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionOrdenesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rami/gestionordenes/GestionOrdenesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rami.gestionordenes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionOrdenesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
