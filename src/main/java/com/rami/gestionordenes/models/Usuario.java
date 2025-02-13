e2b79f962fcdd759fb2c5c804842ff63778f222e
raugusto2903 <ramirol@titamedia.com>
2025-02-12 21:29:14 -0500

    se creo la entidad usuario

--- src/main/java/com/rami/gestionordenes/models/Usuario.java
+++ src/main/java/com/rami/gestionordenes/models/Usuario.java
@@ -1,4 +1,53 @@
 package com.rami.gestionordenes.models;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "usuario")
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idUsuario;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private Boolean enabled;
+
+    @Column(nullable = false)
+    @Getter
+    @Setter
+    private Boolean isAdmin = false;
+    @Column(name = "created_at", updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+    @Column(name = "updated_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedAt;
+
+    public Usuario() {}
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = new Date();
+        this.updatedAt = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = new Date();
+    }
+
+
 }
