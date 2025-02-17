710a38c96554835c2b981c70c21718bf19ecc9cf
raugusto2903 <ramirol@titamedia.com>
2025-02-16 21:34:49 -0500

    se realiza aajuste jwt

--- backend/src/main/java/com/rami/gestionordenes/services/JwtService.java
+++ backend/src/main/java/com/rami/gestionordenes/services/JwtService.java
@@ -1,2 +1,25 @@
-package com.rami.gestionordenes.services;public class JwtService {
+package com.rami.gestionordenes.services;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.Base64;
+
+@Service
+public class JwtService {
+
+    private final Key SECRET_KEY = Keys.hmacShaKeyFor(Base64.getDecoder().decode("supersecretkeythatissufficientlylongforhs256jwt"));
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // Expira en 1 hora
+                .signWith(SECRET_KEY, SignatureAlgorithm.HS256)
+                .compact();
+    }
 }
